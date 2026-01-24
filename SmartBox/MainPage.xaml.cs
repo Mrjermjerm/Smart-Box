9581d8f63890e03e434246d3a49e852a419b31fb
Jeremy Sanders <jeremyedsanders@byui.edu>
2026-01-24 15:37:55 -0700

    pi reads network

--- SmartBox/MainPage.xaml.cs
+++ SmartBox/MainPage.xaml.cs
@@ -1,23 +1,41 @@
-﻿namespace SmartBox;
+﻿
+
+
+namespace SmartBox;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+
+
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	private readonly HttpClient _httpClient = new HttpClient();
+
+
 
 	public MainPage()
 	{
 		InitializeComponent();
+		
+		// Connection to SmartBox
+		CheckSmartFixStatus();
 	}
 
-	private void OnCounterClicked(object? sender, EventArgs e)
+	private async void CheckSmartFixStatus()
 	{
-		count++;
+		try
+        {
+            var response = await _httpClient.GetAsync("http://10.0.0.28:5000/api/devices");
+            var json = await response.Content.ReadAsStringAsync();
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+            SmartBoxDisplay.Text = json; 
+        }
+        catch (Exception)
+        {
+            SmartBoxDisplay.Text = "SmartFix Offline";
+        }
 	}
+
 }
